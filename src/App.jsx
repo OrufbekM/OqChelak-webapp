5b84bbb3ea18535e5ec2336f8cd8db5cd0e126b1
OrufbekM <orufbekmaxmudjonov77@gmail.com>
2026-01-08 13:27:28 +0500

    Setup Chakra UI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Flex justifyContent='center' alignItems='center' height='100vh'>
+      <Button bg='blue.500' color='white'>Fuck Niggers</Button>
+    </Flex>
+  )
+}
+
+export default App
